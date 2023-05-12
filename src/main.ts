83415c9c76d7b1219c0d89bdb917106acf8a84b9
desoga10 <done2stay@gmail.com>
2023-05-12 04:44:36 +0100

    Made Updates

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { DataService } from './app/service/data.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [DataService, importProvidersFrom(HttpClientModule)],
+  providers: [provideHttpClient()],
 }).catch((err) => console.error(err));
